6a4982abd695f9b3a8528023d49f95d07fdc5b6b
Cyvadra <dj@lifesdk.com>
2021-11-06 15:06:36 +0800

    bugfix rpc host settings

--- pkg/filecoin/filecoin.go
+++ pkg/filecoin/filecoin.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"github.com/Cyvadra/sphinx-plugin/pkg/grpc"
+	log "github.com/EntropyPool/entropy-logger"
 )
 
 var host string
@@ -18,13 +19,15 @@ func SetHost(str string) {
 }
 
 func GetBalance(address string) (string, error) {
-	respStr, err := grpc.Request(lotusRpcUrl(host), []string{address}, "Filecoin.WalletBalance")
+	respStr, err := grpc.Request(host, []string{address}, "Filecoin.WalletBalance")
 	if err != nil {
+		log.Errorf(log.Fields{}, "lotusapi request error, %v", err)
 		return "", err
 	}
 	resl := RespGetBalance{}
 	err = json.Unmarshal(respStr, &resl)
 	if err != nil {
+		log.Errorf(log.Fields{}, "cannot unmarshal chain resp, %v", err)
 		return "", err
 	}
 	return resl.Result, err
